b867ada5e033e969de4775df3b719cb980c8e6ef
Cosmin I <phprecode@gmail.com>
2015-01-12 12:13:38 +0200

    Update install_essentials.sh

--- bin/install_essentials.sh
+++ bin/install_essentials.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-sudo apt-get install -y git mc htop vim build-essential python2.7 python-dev python-virtualenv libmysqlclient-dev
+if [ ! -e /usr/lib/apt/methods/https ]; then
+	sudo apt-get update
+	sudo apt-get install -y apt-transport-https
+fi
 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install -y lxc-docker
+
+sudo apt-get install -y \
+curl \
+git \
+mc \
+htop \
+vim \
+build-essential \
+python2.7 \
+python-dev \
+python-virtualenv \
+libmysqlclient-dev \

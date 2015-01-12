d8ad45869d35c73a4b94859133dc194ecf77185f
Cosmin <talpah@gmail.com>
2015-01-12 12:07:22 +0200

    installer tuneup

--- bin/install_essentials.sh
+++ bin/install_essentials.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install git mc htop vim build-essential python2.7 python-dev python-virtualenv libmysqlclient-dev
+sudo apt-get install -y git mc htop vim build-essential python2.7 python-dev python-virtualenv libmysqlclient-dev
 
--- install.sh
+++ install.sh
@@ -2,14 +2,21 @@
 
 me=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+source "${me}/bin/install_essentials.sh"
+
 mkdir -p "${HOME}/bin"
 [ -L "${HOME}/VirtualEnv" ] || ln -s "${me}/VirtualEnv" "${HOME}/VirtualEnv"
 
-for f in ${me}/bin/*; do
-  [ -L "${HOME}/${f}" ] || ln -s "${me}/${f}" "${HOME}/${f}"
+for f in $(ls ${me}/bin/*); do
+  f=$(basename "${f}")
+  [ -L "${HOME}/bin/${f}" ] || ln -s "${me}/bin/${f}" "${HOME}/bin/${f}"
 done
 
 for f in .bashrc .gitignore .profile .venv; do
   rm "${HOME}/${f}"
   ln -s "${me}/${f}" "${HOME}/${f}"
 done
+
+virtualenv "${HOME}/VirtualEnv/local"
+
+echo "You should re-login or source .bashrc"

21c0d744f8bc1c6c17e87ce6e4ce444f3e8ee183
Cosmin I <phprecode@gmail.com>
2015-01-13 10:33:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,19 @@ mkdir -p "${HOME}/bin"
 [ -L "${HOME}/VirtualEnv" ] || ln -s "${me}/VirtualEnv" "${HOME}/VirtualEnv"
 for f in $(ls ${me}/bin/*); do
   f=$(basename "${f}")
+  # No overrides
   [ -L "${HOME}/bin/${f}" ] || ln -s "${me}/bin/${f}" "${HOME}/bin/${f}"
 done
 
 # Actual dot files :)
 for f in .bashrc .gitignore .profile .venv; do
-  mv "${HOME}/${f}" "${me}/backup/${f}"
+  if [[ -f "${me}/backup/${f}" ]]; then
+    # Already backed up
+    rm "${HOME}/${f}"
+  else
+    # Backup
+    mv "${HOME}/${f}" "${me}/backup/${f}"
+  fi
   ln -s "${me}/${f}" "${HOME}/${f}"
 done
 

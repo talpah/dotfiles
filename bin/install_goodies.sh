90f3b6047e9d5e328bed618809f8fcfadc81a320
Cosmin I <talpah@users.noreply.github.com>
2015-09-14 23:02:14 +0300

    Update install_goodies.sh

--- bin/install_goodies.sh
+++ bin/install_goodies.sh
@@ -4,10 +4,7 @@ if [ ! -e /usr/lib/apt/methods/https ]; then
 	sudo apt-get install -y apt-transport-https
 fi
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
-sudo apt-get install -y lxc-docker
+curl -sSL https://get.docker.com/ | sh
 
 sudo apt-get install -y \
 dos2unix \

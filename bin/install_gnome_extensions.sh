288905e066835ec60ec50fef755310144d7b925e
Cosmin I <talpah@users.noreply.github.com>
2016-03-18 21:29:52 +0200

    Added Prevent Legacy Tray Hotspot

--- bin/install_gnome_extensions.sh
+++ bin/install_gnome_extensions.sh
@@ -5,6 +5,6 @@ wget -O "${DIR}/shell-extension-install" https://raw.githubusercontent.com/Nicol
 chmod +x "${DIR}/shell-extension-install"
 GNOMEVERSION=$(gnome-shell --version | sed 's/gnome shell //ig')
 
-for e in 307 584 517 696 885 750 905 779 708 759 817 980 945 1015; do
+for e in 307 584 517 696 885 750 905 779 708 759 817 980 945 979 1015; do
     ${DIR}/shell-extension-install "$GNOMEVERSION" "$e"
 done
